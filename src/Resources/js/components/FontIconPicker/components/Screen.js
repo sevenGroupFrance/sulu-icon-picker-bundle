fa4c44b95a796e42b4bdf079b719ed64939b9770
Tenreiro Benjamin <47473393+tommysonsylverstone@users.noreply.github.com>
2022-02-18 17:07:00 +0100

    Update Screen.js

--- src/Resources/js/components/FontIconPicker/components/Screen.js
+++ src/Resources/js/components/FontIconPicker/components/Screen.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react'
 
 const Screen = (
   {
+    setHidden,
+    setUpDown,
     renderedIconlist,
     shownIcons, setShownIcons,
     elePerPage,
@@ -9,6 +11,67 @@ const Screen = (
     returnIcon
   }
 ) => {
+  const perRow = 5
+  const handleKeyBoardFocus = (e, icon) => {
+    e.preventDefault()
+    if (e.code === "Enter") { returnIcon(icon) }
+    if (e.code === "Backspace") { setHidden(true); setUpDown('down'); }
+  }
+
+  const handleKeyDown = (e, pi, i) => {
+    const parentArray = e.target.parentNode.parentNode.children
+    if (e.code === "ArrowUp") {
+      e.preventDefault();
+      if (pi - 1 < 0) {
+        if (parentArray[parentArray.length - 1].children[i]) {
+          parentArray[parentArray.length - 1].children[i].focus()
+        } else if (parentArray[parentArray.length - 2].children[i]) {
+          parentArray[parentArray.length - 2].children[i].focus()
+        }
+      } else {
+        parentArray[pi - 1].children[i].focus()
+      }
+    }
+
+    if (e.code === "ArrowDown") {
+      e.preventDefault();
+      if (pi + 1 < parentArray.length) {
+        if (parentArray[pi + 1].children[i]) {
+          parentArray[pi + 1].children[i].focus()
+        } else {
+          parentArray[0].children[i].focus()
+        }
+
+      } else {
+        parentArray[0].children[i].focus()
+      }
+    }
+
+    if (e.code === "ArrowLeft" && !e.ctrlKey) {
+      e.preventDefault();
+      if (parentArray[pi].children[i - 1]) {
+        parentArray[pi].children[i - 1].focus()
+      } else {
+        parentArray[pi].children[parentArray[pi].children.length - 1].focus()
+
+      }
+    }
+    if (e.code === "ArrowRight" && !e.ctrlKey) {
+      e.preventDefault();
+      if (parentArray[pi].children[i + 1]) {
+        parentArray[pi].children[i + 1].focus()
+      } else {
+        parentArray[pi].children[0].focus()
+      }
+    }
+  }
+
+  const remapedArray = shownIcons.reduce((acc, curr, i) => {
+    if (!(i % perRow)) {
+      acc.push(shownIcons.slice(i, i + perRow));
+    }
+    return acc;
+  }, []);
 
   useEffect(() => {
     const lastId = currentPage * elePerPage
@@ -18,19 +81,38 @@ const Screen = (
   }, [currentPage, renderedIconlist])
 
   return (
-    <div className="iconpicker-list">
-      {
-        shownIcons.map((icon, i) => {
-          return (
-            <div className="svg-container" key={i} onClick={() => returnIcon(icon)}>
-              <svg>
-                <use xlinkHref={`#${icon}`} />
-              </svg>
-            </div>
-          )
-        })
-      }
-    </div>
+    <table className="iconpicker-list">
+      <tbody>
+        {
+          remapedArray.map((array, pi) => {
+            return (
+              <tr key={pi}>
+                {
+                  array.map((icon, i) => {
+                    return (
+                      <td
+                        className="svg-container"
+                        key={i}
+                        onClick={() => returnIcon(icon)}
+                        tabIndex={0}
+                        onKeyUp={(e) => handleKeyBoardFocus(e, icon)}
+                        onKeyDown={e => handleKeyDown(e, pi, i)}
+                        onBlur={e => e.target.style.boxShadow = ""}
+                        title={icon}
+                      >
+                        <svg>
+                          <use xlinkHref={`#${icon}`} />
+                        </svg>
+                      </td>
+                    )
+                  })
+                }
+              </tr>
+            )
+          })
+        }
+      </tbody>
+    </table>
   )
 }
 
